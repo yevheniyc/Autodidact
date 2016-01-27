87279535b9f249395b417f6dcb50c3dabc1398e9
whitehat <yevheniyc@gmail.com>
2016-01-27 07:23:50 -0800

    C_Estell: to lowercase ultimate.

--- C_Programs_Examples/17_character_processing/lowercase3.c
+++ C_Programs_Examples/17_character_processing/lowercase3.c
@@ -1,3 +1,14 @@
 /*
 	lowercase3.c - Yevheniy Chuba - 01/26
+	Even better way of lowercase program - ctypes.h
 */
+
+#include <stdio.h>
+#include <ctype.h>
+
+main() {
+	char ch;
+
+	while ((ch = getchar()) != EOF) 
+		putchar(tolower(ch)); // if ch is not upper case, function returns ch
+}
