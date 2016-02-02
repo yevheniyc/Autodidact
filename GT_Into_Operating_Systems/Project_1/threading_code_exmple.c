a4ca0e806ae0d977dfe2991d3a4356ff603dbbfb
whitehat <yevheniyc@gmail.com>
2016-02-02 01:36:46 -0800

    gtpr1: code reorganization and better commenting
    - reminds me of bad javascript

--- GT_Into_Operating_Systems/Project_1/threading_code_exmple.c
+++ GT_Into_Operating_Systems/Project_1/threading_code_exmple.c
@@ -19,6 +19,7 @@
 #define NOTOK_404   "HTTP/1.0 404 Not Found\nContent-Type:text/html\n\n"
 #define MESS_404    "<html><body><h1>FILE NOT FOUND</h1></body></html>"
 
+////////////// VARIABLES ////////////////////////////////////
 sem_t sem;
 int free_thread;
 int sched_flag = 0, 
@@ -29,7 +30,10 @@ pthread_t t_serve;
 pthread_mutex_t qmutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t sthread_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
+/////////////////////////////////////////////////////////////
 
+
+/////////////// STRUCTS /////////////////////////////////////
 struct request {
 	int acceptfd;
 	int size;
@@ -37,8 +41,18 @@ struct request {
 	unsigned int cli_ipaddr;
 	char time_arrival[1024];
 	char in_buf[2048];
-} r2; 
+} r2;
 
+//queue structre
+struct node {
+	struct request r;	
+	struct node *link;
+} *new, *temp, *p, *front=NULL, *rear=NULL;
+typedef struct node N;
+/////////////// END STRUCTS //////////////////////////////////
+
+
+////////////// PROTOTYPES ///////////////////////////////////
 // queue function declarations;
 void insertion(int, char*, int, unsigned int, char*, char*);	
 //void insertion(int,string, int);
@@ -46,18 +60,122 @@ struct request extract_element();
 struct request removesjf();
 void display();
 void print_help_options();
+////////////// END PROTOTYPES ///////////////////////////////
 
-//queue structre
-struct node {
-	struct request r;	
-	struct node *link;
-} *new, 
-  *temp, 
-  *p, 
-  *front=NULL, 
-  *rear=NULL;
-typedef struct node N;
 
+///////// MAIN /////////////////////////
+int main(int argc, char *args[]) {
+	int thread_status[10];     	
+	pthread_t t_listener,
+			  t_scheduler,
+			  t_serve[10];
+	int sockfd,
+		ids;
+	char *dir;
+	file = malloc(sizeof(char *));
+	dir = malloc(sizeof(char *));
+	
+	int portnum = 8080,
+		threadnum = 4,
+		sleep_time = 60;
+	int i;
+	int help_flag = 0,
+		dir_flag = 0,
+		time_flag, 
+		threadnum_flag = 0;
+	
+	// Parser code
+	for (i = 0; i < argc; i++) {
+		//printf("\ncomparing%d",i);
+		if( strcmp(args[i], "-h") == 0) {
+			help_flag=1; }
+		else if (strcmp(args[i], "-n") == 0) {
+			threadnum = atoi(args[i+1]);
+		} else if(strcmp(args[i], "-d") == 0) {
+			debug_flag=1;
+			threadnum=1;
+		} else if(strcmp(args[i], "-l") == 0) {
+			log_flag=1;
+			file=args[i+1];
+		} else if(strcmp(args[i], "-p") == 0)
+		{
+			portnum=atoi(args[i+1]);
+		} else if(strcmp(args[i], "-r") == 0)
+		{
+			dir_flag=1;		
+			dir=args[i+1];
+		} else if(strcmp(args[i], "-t")==0)
+		{
+			time_flag=1;		
+		 	sleep_time=atoi(args[i+1]);
+		} else if(strcmp(args[i], "-s")==0)
+		{
+			if (strcmp(args[i+1],"FCFS") == 0)		
+				sched_flag=0;
+			else if(args[i+1], "SJF")
+				sched_flag=1;
+			else
+				printf("Please enter a proper scheduling algorithm");
+		}
+	}
+	
+	//free_thread=threadnum;
+	sem_init(&sem, 0, threadnum);
+
+	//printf( "\n debug : %d, help: %d, log: %d, file name : %s port num : %d, dir : %d dir name: %s, time :%d ,thread num : %d, sched : %d",
+	//			  debug_flag, help_flag, log_flag, file, portnum, dir_flag, dir, sleep_time, threadnum, sched_flag);	
+	//Parser code ends
+
+	if (help_flag == 1) {	// printing help options and exit if -h option is specified
+		print_help_options();
+		exit(1);
+	} else if(dir_flag == 1) //changing directory if -d option is specified
+	{
+		if(chdir(dir) < 0) {
+			perror("\ndirectory doesnt exist");
+			exit(1);
+		}
+	}
+	
+
+	struct sockaddr_in serv_addr;
+	//printf("before socket creation");
+	sockfd = socket(AF_INET, SOCK_STREAM,0); //creation of socket	
+	//printf("\n after socket creation socket id is %d", sockfd);
+	if (sockfd < 0) 
+        perror("error creating socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portnum);
+	//printf("before bind");
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) //binding socket
+		perror("binding error");
+	//printf("\nafter bind");
+	
+	// creating a pool
+    int w;
+	for(w=0; w < threadnum; w++) {
+		pthread_create(&t_serve[w], NULL, &thread_serve, NULL); 
+ 	} 
+     
+	ids=sockfd;
+	//printf("\n before creating scheduler thread");
+	//printf("\nbefore creating thread sockfd is %d",ids);
+	pthread_create(&t_listener,NULL,&thread_listen,&ids); //creating listener thread
+	sleep(sleep_time);	// putting scheduler to sleep
+	pthread_create(&t_scheduler,NULL,&thread_scheduler,&sched_flag); //creating scheduler thread
+    pthread_join(t_listener, NULL);
+	pthread_join(t_scheduler, NULL);
+	//printf("\nafter join in main");
+	display();
+	close(sockfd);
+	return 0;
+}
+///////// END MAIN /////////////////////
+
+
+///////////// FUNCTIONS //////////////////
 void display() {
 	if(front==NULL)
 		printf("\nempty queue");
@@ -116,7 +234,6 @@ struct request extract_element() {
 	}
 }
  
-
 struct request removesjf(int num) { 
 	printf("\nentered removesjf");
 	if(front==NULL)
@@ -140,7 +257,6 @@ struct request removesjf(int num) {
 	}
 }
 // end of queue functions
-
 
 // thread pool function
 void *thread_serve() {
@@ -326,8 +442,6 @@ void *thread_scheduler(void *arg) {
 	}
 } // end thread scheduler
 
-
-
 // Listening and queueing thread
 void *thread_listen(void *arg) {
 	unsigned int sockfd = *((unsigned int*)arg);
@@ -425,117 +539,6 @@ void *thread_listen(void *arg) {
 	}				
 }
 
-
-int main(int argc, char *args[]) {
-	int thread_status[10];     	
-	pthread_t t_listener,
-			  t_scheduler,
-			  t_serve[10];
-	int sockfd,
-		ids;
-	char *dir;
-	file = malloc(sizeof(char *));
-	dir = malloc(sizeof(char *));
-	
-	int portnum = 8080,
-		threadnum = 4,
-		sleep_time = 60;
-	int i;
-	int help_flag = 0,
-		dir_flag = 0,
-		time_flag, 
-		threadnum_flag = 0;
-	
-	// Parser code
-	for (i = 0; i < argc; i++) {
-		//printf("\ncomparing%d",i);
-		if( strcmp(args[i], "-h") == 0) {
-			help_flag=1; }
-		else if (strcmp(args[i], "-n") == 0) {
-			threadnum = atoi(args[i+1]);
-		} else if(strcmp(args[i], "-d") == 0) {
-			debug_flag=1;
-			threadnum=1;
-		} else if(strcmp(args[i], "-l") == 0) {
-			log_flag=1;
-			file=args[i+1];
-		} else if(strcmp(args[i], "-p") == 0)
-		{
-			portnum=atoi(args[i+1]);
-		} else if(strcmp(args[i], "-r") == 0)
-		{
-			dir_flag=1;		
-			dir=args[i+1];
-		} else if(strcmp(args[i], "-t")==0)
-		{
-			time_flag=1;		
-		 	sleep_time=atoi(args[i+1]);
-		} else if(strcmp(args[i], "-s")==0)
-		{
-			if (strcmp(args[i+1],"FCFS") == 0)		
-				sched_flag=0;
-			else if(args[i+1], "SJF")
-				sched_flag=1;
-			else
-				printf("Please enter a proper scheduling algorithm");
-		}
-	}
-	
-	//free_thread=threadnum;
-	sem_init(&sem, 0, threadnum);
-
-	//printf( "\n debug : %d, help: %d, log: %d, file name : %s port num : %d, dir : %d dir name: %s, time :%d ,thread num : %d, sched : %d",
-	//			  debug_flag, help_flag, log_flag, file, portnum, dir_flag, dir, sleep_time, threadnum, sched_flag);	
-	//Parser code ends
-
-	if (help_flag == 1) {	// printing help options and exit if -h option is specified
-		print_help_options();
-		exit(1);
-	} else if(dir_flag == 1) //changing directory if -d option is specified
-	{
-		if(chdir(dir) < 0) {
-			perror("\ndirectory doesnt exist");
-			exit(1);
-		}
-	}
-	
-
-	struct sockaddr_in serv_addr;
-	//printf("before socket creation");
-	sockfd = socket(AF_INET, SOCK_STREAM,0); //creation of socket	
-	//printf("\n after socket creation socket id is %d", sockfd);
-	if (sockfd < 0) 
-        perror("error creating socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portnum);
-	//printf("before bind");
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) //binding socket
-		perror("binding error");
-	//printf("\nafter bind");
-	
-	// creating a pool
-    int w;
-	for(w=0; w < threadnum; w++) {
-		pthread_create(&t_serve[w], NULL, &thread_serve, NULL); 
- 	} 
-     
-	ids=sockfd;
-	//printf("\n before creating scheduler thread");
-	//printf("\nbefore creating thread sockfd is %d",ids);
-	pthread_create(&t_listener,NULL,&thread_listen,&ids); //creating listener thread
-	sleep(sleep_time);	// putting scheduler to sleep
-	pthread_create(&t_scheduler,NULL,&thread_scheduler,&sched_flag); //creating scheduler thread
-    pthread_join(t_listener, NULL);
-	pthread_join(t_scheduler, NULL);
-	//printf("\nafter join in main");
-	display();
-	close(sockfd);
-	return 0;
-}
-
-
 void print_help_options() {
 	printf("\n−d : Enter debugging mode. That is, do not daemonize, only accept one connection at a \n 
 				  time and enable logging to stdout. Without this option, the web server should run as a daemon process in the background. 
@@ -548,3 +551,4 @@ void print_help_options() {
 		    				The default should be 4 execution threads. 
 		    \n−s sched: Set the scheduling policy. It can be either FCFS or SJF. The default will be FCFS.");
 }
+/////////// END FUNCTIONS /////////////////
