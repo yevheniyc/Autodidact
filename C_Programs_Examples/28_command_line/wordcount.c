a1c4848d0c10ad15d5ffb00f9362a0a8cb07acdf
whitehat <yevheniyc@gmail.com>
2016-02-02 00:01:31 -0800

    c_estell: starting wordcount similar to unix's wc

---
+++ C_Programs_Examples/28_command_line/wordcount.c
@@ -0,0 +1,16 @@
+/*
+	wordcount.c - Yevheniy Chuba - 01/31/2016
+	Unix wc "workalike" program, complete with files and flags.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define YES 1
+#define NO 0
+#define UsageMesg "Usage: %s [-lwc] [filelist]\n"
+#define OpenMesg  "%s: %s: No such file or directory\n"
+
+main(int argc, char *argv[]) {
+	
+}
