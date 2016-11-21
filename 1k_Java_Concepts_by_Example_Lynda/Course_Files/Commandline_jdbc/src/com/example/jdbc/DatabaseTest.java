96d53eeae6eaf0b4e39539d860ccdda0ef899c13
redhat <yevheniyc@gmail.com>
2016-11-20 16:28:23 -0800

    jce: writing properties file programmatically.

--- 1k_Java_Concepts_by_Example_Lynda/Course_Files/Commandline_jdbc/src/com/example/jdbc/DatabaseTest.java
+++ 1k_Java_Concepts_by_Example_Lynda/Course_Files/Commandline_jdbc/src/com/example/jdbc/DatabaseTest.java
@@ -1,10 +1,25 @@
 package com.example.jdbc;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class DatabaseTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Generate Properties file programmatically
+	private String dbUrl;
+	private String dbUser;
+	private String dbPassword;
+	
+	public void writeProperties() throws IOException {
+		Properties prop = new Properties();
+		prop.setProperty("dbUrl", "localhost");
+		prop.setProperty("dbUser", "username");
+		prop.setProperty("dbPassword", "password");
+		
+		try (OutputStream out = new FileOutputStream("project.properties")) {
+			prop.store(out, "Database Properties File");
+		}
 	}
 
 }
