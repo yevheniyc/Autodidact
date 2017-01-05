6feb93992ca87912431c17330718b17b705a7ee3
whitehat <yevheniyc@gmail.com>
2017-01-04 21:46:20 -0800

    ret: adding and calling methods to classes/components

--- 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch03/03_04/start/src/components/SkiDayCount.js
+++ 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch03/03_04/start/src/components/SkiDayCount.js
@@ -2,6 +2,12 @@ import React from 'react'
 import '../stylesheets/ui.scss'
 
 export const SkiDayCount = React.createClass({
+	percentToDecimal(decimal) {
+		return ((decimal * 100) + '%')
+	},
+	calcGoalProgress(total, goal) {
+		return this.percentToDecimal(total/goal)
+	},
 	render() {
 		return (
 			<div className="ski-day-count">
@@ -18,7 +24,12 @@ export const SkiDayCount = React.createClass({
 					<span>days</span>
 				</div>
 				<div>
-					<span>{this.props.goal}</span>
+					<span>
+						{this.calcGoalProgress(
+							this.props.total,
+							this.props.goal
+						)}
+					</span>
 				</div>
 			</div>
 		)
