b9b1b32bb05e6d9460fbf1752e075efdded14d3c
whitehat <yevheniyc@gmail.com>
2017-01-05 14:42:51 -0800

    ret: using stateless functional components implementation

--- 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch03/03_06/start/src/components/SkiDayCount.js
+++ 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch03/03_06/start/src/components/SkiDayCount.js
@@ -1,37 +1,34 @@
-import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends Component {
-	percentToDecimal(decimal) {
-		return ((decimal * 100) + '%')
-	}
-	calcGoalProgress(total, goal) {
-		return this.percentToDecimal(total/goal)
-	}
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{this.props.total}</span>
-					<span>days</span>
-				</div>
-				<div className="powder-days">
-					<span>{this.props.powder}</span>
-					<span>days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{this.props.backcountry}</span>
-					<span>days</span>
-				</div>
-				<div>
-					<span>
-						{this.calcGoalProgress(
-							this.props.total, 
-							this.props.goal
-						)}
-					</span>
-				</div>
-			</div>
-		)
-	}
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+	<div className="ski-day-count">
+		<div className="total-days">
+			<span>{total}</span>
+			<span>days</span>
+		</div>
+		<div className="powder-days">
+			<span>{powder}</span>
+			<span>days</span>
+		</div>
+		<div className="backcountry-days">
+			<span>{backcountry}</span>
+			<span>days</span>
+		</div>
+		<div>
+			<span>
+				{calcGoalProgress(
+					total, 
+					goal
+				)}
+			</span>
+		</div>
+	</div>
+)
