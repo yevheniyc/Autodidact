0864a0fb0e1e745dd142c43ef36cc3e5dfcdb7be
whitehat <yevheniyc@gmail.com>
2017-01-05 15:56:59 -0800

    ret: implementing SkiDayList and SkiDayRow components

--- 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch04/04_01/start/src/index.js
+++ 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch04/04_01/start/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount total={50}
-				 powder={20}
-				 backcountry={10}
-				 goal={100}/>,
+	// SkiDayList represents a table, wich days representing each row
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw Valley",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirkwood",
+				date: new Date("3/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Mt. Tallac",
+				date: new Date("4/2/2016"),
+				powder: true,
+				backcountry: false
+			}
+		]	
+	}/>,
 	document.getElementById('react-container')
 )
