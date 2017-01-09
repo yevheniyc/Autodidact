74d2520649236ed8eeed11d9770e5168f56d5378
whitehat <yevheniyc@gmail.com>
2017-01-08 17:07:07 -0800

    ret: add a members list

--- 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch07/07_02/start/src/components/ui/MemberList.js
+++ 1l_React_ET_Lynda/Ex_Files_React_EssT/Ch07/07_02/start/src/components/ui/MemberList.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
+import Member from './Member'
 
 class MemberList extends Component {
     constructor(props) {
@@ -26,9 +27,16 @@ class MemberList extends Component {
     }
 
     render() {
+        const {members} = this.state
         return (
             <div className="member-list">
             	<h1>Society Members</h1>
+                {members.map(
+                    (data, i) => 
+                        <Member key={i}
+                                onClick={emai => console.log(email)}
+                                {...data}/> // send all data as properties to each member
+                )}
             </div>
         )    
    }     
